17764ec27d464d66ac07e97d347c09fb1d4e723c
jsy8612 <jsy8612@gmail.com>
2016-02-10 13:14:45 +0900

    eee

--- suri_project/app/controllers/Application.scala
+++ suri_project/app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
 
   def index = Action {  implicit request =>
-    Ok(views.html.index("TESTTTTTTTTTTTTTTTTTTTTTTTT"))
+    Ok(views.html.index("SILVEEEEEEEEEEEEEEEEEEEEEEEE"))
   }
 
 }
