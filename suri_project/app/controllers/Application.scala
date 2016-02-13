a3d6ae4288b5bdd6e566f6db8fbaae920af1ddfc
jsy8612 <jsy8612@gmail.com>
2016-02-13 17:03:58 +0900

    Revert "IU-15.0.3 <Pang@Pang-PC Update vcs.xml"
    This reverts commit b9e72ac3a910aabbbbc056dd34e361b890e0298f.
    
    # Conflicts:
    #	vcs.xml

--- suri_project/app/controllers/Application.scala
+++ suri_project/app/controllers/Application.scala
@@ -1,30 +1,30 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 import play.api.db._
 import play.api.Play.current
 
 object Application extends Controller {
 
-  def index = Action {
-    var outString = "Number is "
-    val conn = DB.getConnection()
-    try {
-      val stmt = conn.createStatement
-      val rs = stmt.executeQuery("SELECT COL2 FROM TEST_TABLE ")
-      while (rs.next()) {
-        outString += rs.getString("COL2")
-      }
-    } finally {
-      conn.close()
-    }
-    Ok(outString)
-  }
-
-//  def index = Action { implicit request =>
-//    Ok(views.html.index())
+//  DB 연결 테스트
+//  def index = Action {
+//    var outString = "Number is "
+//    val conn = DB.getConnection()
+//    try {
+//      val stmt = conn.createStatement
+//      val rs = stmt.executeQuery("SELECT 9 AS COL2 FROM DUAL")
+//      while (rs.next()) {
+//        outString += rs.getString("COL2")
+//      }
+//    } finally {
+//      conn.close()
+//    }
+//    Ok(outString)
 //  }
+
+  def index = Action { implicit request =>
+    Ok(views.html.index())
+  }
 
   def main = Action { implicit request =>
     Ok(views.html.main())
